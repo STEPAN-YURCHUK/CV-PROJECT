46a3067cc8f0f6087ef0decaf445a79cd6618bbb
Степан Юрчук <yurchuk@MacBook-Air-Stepan.local>
2023-05-04 14:51:41 +0300

    Refactoring app

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -15,22 +15,30 @@ export class AuthService {
 	) {}
 
 	async registerUsers(dto: CreateUserDTO): Promise<CreateUserDTO> {
-		const exitsUser = await this.usersService.findUserByEmail(dto.email)
-		if (exitsUser) throw new BadRequestException(AppError.USER_EXIST)
-		return this.usersService.createUser(dto)
+		try {
+			const exitsUser = await this.usersService.findUserByEmail(dto.email)
+			if (exitsUser) throw new BadRequestException(AppError.USER_EXIST)
+			return this.usersService.createUser(dto)
+		} catch (e) {
+			throw new Error(e)
+		}
 	}
 
 	async loginUser(dto: UserLoginDTO): Promise<AuthUserResponse> {
-		const existUser = await this.usersService.findUserByEmail(dto.email)
-		if (!existUser) throw new BadRequestException(AppError.USER_NOT_FOUND)
-		const validatePassword = await bcrypt.compare(
-			dto.password,
-			existUser.password,
-		)
-		if (!validatePassword) throw new BadRequestException(AppError.WRONG_DATA)
-		const user = await this.usersService.publicUser(dto.email)
-		const token = await this.tokenService.generateJwtToken(user)
+		try {
+			const existUser = await this.usersService.findUserByEmail(dto.email)
+			if (!existUser) throw new BadRequestException(AppError.USER_NOT_FOUND)
+			const validatePassword = await bcrypt.compare(
+				dto.password,
+				existUser.password,
+			)
+			if (!validatePassword) throw new BadRequestException(AppError.WRONG_DATA)
+			const user = await this.usersService.publicUser(dto.email)
+			const token = await this.tokenService.generateJwtToken(user)
 
-		return { user, token }
+			return { user, token }
+		} catch (e) {
+			throw new Error(e)
+		}
 	}
 }
